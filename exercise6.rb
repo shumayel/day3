ba5c1303dac3270639c559a1241ddc8d04abbcbe
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-10 13:56:49 -0500

    Appended and added Banana if else statement

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,7 @@
 grocery_list = ["filet mignon", "bison", "organic meats", "crab", "truffles"]
 
+grocery_list << "brownrice"
+
 def asterik(list)
 list.each do |grocery_item|
 puts "*" + grocery_item
@@ -7,3 +9,10 @@ end
 end
 
 asterik(grocery_list)
+puts "This is the total number of items on the list: " + grocery_list.length.to_s
+
+if grocery_list == "banana"
+	puts "You don't need bananas, bro."
+else
+	puts "YOU NEED BANANAS!"
+end
