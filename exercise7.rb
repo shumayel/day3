eaad4bc4a28bb53a250d97f40db99c9ba00bc738
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-10 14:23:05 -0500

    After adding 4th cohort, and and increasing cohort number by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -4,6 +4,13 @@ students = {
 	:cohort3 => 22
 }
 
+
+students[:cohort4] = 43
+
+students.keys
+
+
+
 students.each do |key, value|
-	puts "#{key}: #{value} students"
+	puts "#{key}: #{value*1.05} students"
 end
